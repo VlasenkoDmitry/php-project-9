9626c91d61474e4836bf150ab7da365fd8827f5a
DzmitryV <vlasenko.dmitry1@gmail.com>
2024-04-13 13:20:09 +0300

    build: add css, phtml files, laid out the basis of the start page

--- public/index.php
+++ public/index.php
@@ -3,12 +3,20 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
+$router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+
+    return $this->get('renderer')->render($response, 'main/index.phtml');
 });
 $app->run();
